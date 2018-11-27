493ff182e5271d6e330b4fb2ebc9ec657d454128
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-27 15:02:41 +0100

    Updates

---
+++ precache-manifest.52629a0ca160150c2e197df8d7dd59f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "b7ec2a28289b1748c369",
+    "url": "/todo-app/static/js/main.b7ec2a28.chunk.js"
+  },
+  {
+    "revision": "fb4a947b1333c8556745",
+    "url": "/todo-app/static/js/1.fb4a947b.chunk.js"
+  },
+  {
+    "revision": "b7ec2a28289b1748c369",
+    "url": "/todo-app/static/css/main.61196670.chunk.css"
+  },
+  {
+    "revision": "03edf1ce452c61340e2ae487bf3c2f20",
+    "url": "/todo-app/index.html"
+  }
+];
