6bb636a0692f1adabf5f22831a824f0e89efe255
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-27 11:02:07 +0100

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import { List, ListItem } from 'material-ui/List';
 
 class App extends Component {
   state = {
@@ -10,19 +11,39 @@ class App extends Component {
     ],
     taskName: ''//searched taskName
   }
+  handleChange = (event) => {
+    this.setState({ taskName: event.target.value })
+  }
+  handleClick = (event) => {
+    if (this.state.taskName !== '') {
+      let tasks = this.state.tasks;
+      tasks.push({ taskName: this.state.taskName, completed: false });
+      this.setState({ tasks, taskName: '' })
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <div>
-          <TextField hintText="Enter your task here" />
-          <RaisedButton label="Add" primary={true} />
+          <TextField
+            onChange={this.handleChange}
+            value={this.state.taskName}
+            fullWidth={true}
+            hintText="Enter your task here" />
+          <RaisedButton
+            label="Add"
+            primary={true}
+            fullWidth={true}
+            onClick={this.handleClick}
+          />
         </div>
-        <div>
+        <List>
+
           {this.state.tasks.map((task, index) => (
             <div>{task.taskName}</div>
           ))}
-        </div>
+        </List>
 
       </div>
     )
